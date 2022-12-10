2653dc05ef08cde5be3c0d4ecdc7789cc1d379a1
Curbelman <111905398+Curbelman@users.noreply.github.com>
2022-12-10 11:24:43 +0100

    adds recipe grid on the index page (home) and includes a link to the recipe page. Vira please check if the redirection is correct for the recipe routes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const recipe = require('../models/Recipe.model');
+const data = require('.db/index'); // maybe dont need
 const router = express.Router();
 
 /* GET home page */
 router.get("/", (req, res, next) => {
-  res.render("index");
+  Recipe
+  .find()
+  .populate("creator")
+  .then(recipes => {
+    res.render("index", { recipes })
+  })
 });
 
 module.exports = router;
