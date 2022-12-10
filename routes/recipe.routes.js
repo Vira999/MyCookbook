0c915d085c799425d2943fb12ecc0a4a9553a773
Vira Sholinian <105912497+Vira999@users.noreply.github.com>
2022-12-10 12:09:06 +0100

    Add files via upload

--- routes/recipe.routes.js
+++ routes/recipe.routes.js
@@ -1,36 +1,35 @@
 const express = require('express');
-const recipe = require('../models/Recipe.model');
 const router = express.Router();
+
+const { isLoggedIn, isOwner } = require('../middleware/')//add file where located
+const fileUploader = require("../config/cloudinary.config");
+
+const recipe = require('../models/Recipe.model');
 const comment = require('../models/Comments.model'); 
 const data = require('.db/index'); // maybe dont need
 
  
 // GET route to retrieve and display all the recipes
-router.get('/', (req, res) => {
-    
-    Recipe.find()
-        .then(dbRecipes => { //choose name on db folder
-            res.render('../recipe.hbs', { recipes: dbRecipes })
-        })
-        .catch(err => console.log(err))
+router.get('/', isLoggedIn, (req, res) => {
+  res.render('recipe/index');
 });
 
 
-router.get('/search', (req, res) => {
+router.get('/search', isLoggedIn,  (req, res) => {
         const { recipeName } = req.query;
 
-        Recipe.findOne({title: RecipeName})
-            .then(foundRecipe => {
-                res.render('../views/recipe.hbs', {singleRecipe: foundBrecipe})
+        recipe.findOne({title: recipeName})
+            .then(foundByRecipe => {
+                res.render('../views/recipe-detail.hbs', {singleRecipe: foundByRecipe})
             })
             .catch(err => console.log(err))
 })
 
-router.get('/create', (req, res) => {
+router.get('/create', isLoggedIn, (req, res) => {
     res.render('../views/recipe.hbs')
 })
 
-router.post('/create', (req, res) => {
+router.post('/create', isLoggedIn, (req, res) => {
     console.log(req.body)
     const { title,
          time,
@@ -40,7 +39,7 @@ router.post('/create', (req, res) => {
         ingredients,
         instruction} = req.body;
     
-   Recipe.create({ 
+   recipe.create({ 
    title,
    time,
    date,
@@ -51,17 +50,42 @@ router.post('/create', (req, res) => {
         .then(() => res.redirect('../views/recipe.hbs'))
         .catch(err => console.log(err))
 })
+//READ: Recipe details
+router.get('/recipes/:recipeId', (req, res, next) => {
+  const id = req.params.recipeId;
 
-router.get('/:recipeId', (req, res) => {
+  Recipe.findById(id)
+  .then(recipeDetails => {
+      res.render('recipes/recipe-details', recipeDetails);
+  })
+  .catch(err => {
+      console.log('error getting recipe details from DB', err);
+      next(err);
+  });
+});
+
+router.get('/:recipeId', isLoggedIn, (req, res) => {
     const { recipeId } = req.params;
    
-    Recipe.findById(recipeId)
+    recipe.findById(recipeId)
         .then(recipeFound => {
             console.log('recipeFound', recipeFound)
             res.render('../views/recipe.hbs', {singleRecipe: recipeFound})
         })
         .catch(err => console.log(err))
 })
+
+//DELETE FROM EDIT PAGE
+router.post('/recipes/:recipeId/delete', isLoggedIn, (req, res, next) => {
+    Recipe.findByIdAndDelete(req.params.recipeId)
+    .then(() => {
+        res.redirect('/recipes');
+    })
+    .catch(err => {
+        console.log('error deleting', err);
+        next();
+    });
+});
 
 router.get('/comments/:id', (req, res) => {
  
