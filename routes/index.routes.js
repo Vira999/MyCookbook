dcc906d7e0513eeff958c66dd0f8be5f0a1fe163
Curbelman <111905398+Curbelman@users.noreply.github.com>
2022-12-10 11:54:23 +0100

    adds conditional navigation bar in layout.hbs depeding on whether the user is logged in or not

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -5,12 +5,25 @@ const router = express.Router();
 
 /* GET home page */
 router.get("/", (req, res, next) => {
+  const loggedInNavigation = req.session.hasOwnProperty('currentUser');
+  const userId = req.session.currentUser._id 
+
+  if(loggedInNavigation){
   Recipe
+  .find()
+  .populate("creator")
+  .then(recipes => {
+    res.render("index", { recipes, loggedInNavigation, userId })
+  })
+  }
+  else {
+    Recipe
   .find()
   .populate("creator")
   .then(recipes => {
     res.render("index", { recipes })
   })
+  }
 });
 
 module.exports = router;
