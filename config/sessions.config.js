d1aa0baf7c37428bc0ae78ba23d1c0bb264682b0
Curbelman <111905398+Curbelman@users.noreply.github.com>
2022-12-10 13:41:14 +0100

    adds recipe routes to app.js

---
+++ config/sessions.config.js
@@ -0,0 +1,19 @@
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+
+module.exports = app => {
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24, // 24h
+      },
+      store: new MongoStore({
+        mongoUrl: 'mongodb://localhost/myCookbook',
+        ttl: 60 * 60 * 24 // 60sec * 60min * 24h => 1 day      
+      })
+    })
+  );
+};
