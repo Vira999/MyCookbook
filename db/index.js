4fa38b39c1dab4ece677f84a853b53c0c0fccb95
Vira Sholinian <105912497+Vira999@users.noreply.github.com>
2022-12-14 16:33:17 +0100

    Add files via upload

--- db/index.js
+++ db/index.js
@@ -5,11 +5,9 @@ const mongoose = require("mongoose");
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
-const MONGO_URI =
-  process.env.MONGODB_URI || "mongodb+srv://Vira:<01122022>@cluster0.6qjfiiw.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose
-  .connect(MONGO_URI)
+  .connect(process.env.MONGODB_URI)
   .then((x) => {
     const databaseName = x.connections[0].name;
     console.log(`Connected to Mongo! Database name: "${databaseName}"`);
