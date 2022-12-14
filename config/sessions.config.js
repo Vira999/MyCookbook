afc7cb16bc7748e00de5f4f9354f2aaadd40d767
Vira Sholinian <105912497+Vira999@users.noreply.github.com>
2022-12-14 16:30:45 +0100

    Add files via upload

--- config/sessions.config.js
+++ config/sessions.config.js
@@ -11,7 +11,9 @@ module.exports = app => {
         maxAge: 1000 * 60 * 60 * 24, // 24h
       },
       store: new MongoStore({
-        mongoUrl: 'mongodb+srv://Vira:<01122022>@cluster0.6qjfiiw.mongodb.net/?retryWrites=true&w=majority',
+
+        mongoUrl: process.env.MONGODB_URI,
+
         ttl: 60 * 60 * 24 // 60sec * 60min * 24h => 1 day      
       })
     })
